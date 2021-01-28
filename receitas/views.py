9ff2d7beb659fbc55ebd01a15fac61f14870997e
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-28 14:21:52 -0300

    ataulização buscas

--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 def index(request):
-    receitas = Receita.objects.all()
-    
+    receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+                               ##ordena por data mais recente, tem um filtro de publicada ou nao
     dados = {
         'receitas': receitas
     }
@@ -17,3 +17,11 @@ def receita(request, receita_id):
     }
 
     return render(request,'receita.html', receita_a_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains)
+    return render(request, 'buscar.html')
