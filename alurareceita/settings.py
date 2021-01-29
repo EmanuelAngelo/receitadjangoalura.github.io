90419c7ee3d2497c28b20d03d3f7dae93d361c37
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-29 17:01:21 -0300

    ataulizando terceio modolo

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pessoas',
+    'usuarios',
     'receitas',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'receitas/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
